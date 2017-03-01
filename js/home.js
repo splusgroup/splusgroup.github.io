3fd5139a388aefe8810c79f11163ea80af5c359c
Edward Rees <erees.hk@gmail.com>
2017-03-01 21:47:54 +0800

    update of index text

--- js/home.js
+++ js/home.js
@@ -23,11 +23,17 @@ $(document).ready(function(){
 	
 	
 	function First(){
-			$("#image").html("<img src='/css/introbanner.png'>")
-			$("#text").html("<span>Your comfort is our passion.  Especially in the home - your home.  We put people first in everything we do to deliver an enhanced day-to-day living experience for elder residents by creating the best lifestyle possible within the familiarity of your own home.  Our goal - provide the choice for active elders to enjoy the personal responsibility of living at home while savoring independence, enhanced well being and improved quality of life with a specially equipped home.   We continuously work to know more about the needs of elder residents and how they want to live and bring those solutions to your door; for today and the future.</span><br /><br /><br/><br/> <a href='discussion.html' style='float: right'>Discussion Page</a>");
+			$("#text2").html("<span>提供舒適優閒及安全的家居生活，是我們服務的首要宗旨 。<br /><br /></span><span id='indent'>我們提供的服務以人為本 。因此，與一般承建商的裝置服務有別，我們提供專業人士作家訪 ，從而了解每使用者的不同個別需要 ，繼而提供各種安全設施，但求讓使用者得到適用的裝置 ，從而在熟悉的環境享受獨立  ，自主  ，及安全無憂的生活。</span><br /><br /><br /><br />");
+			$("#text").html("<span>Your comfort is our passion.  Especially in the home - your home.  <br /><br /></span><span id='indent'>We put people first in our service with an approach differing from normal contracting.  S+ provide professional consultation to understand your special needs, provide a range of safety products accommodating those needs and install them with that customized purpose in your home.   Our aim is to cater for everyone’s differing and personal requirement so they can comfortably enjoy staying in a familiar place - independent and safe.</span><br /><br /><br/><br/> <a href='discussion.html' style='float: right'>Discussion Page</a>");
 			$("#text").css("font-size", "20px");
+			$("#text2").css("font-size", "20px");
+			$("#text2").css("color", "#606060");
+			$("#text2").css("text-align", "left");
+			$("#text2").css("text-indent", "50px");
 			$("#text").css("text-indent", "50px");
+			$("#indent").css("text-indent", "20px");
 			$("#bottomScript").hide();
+			$("#text2").show();
 			$("#chineseBottom").hide();
 			$("#chineseText").hide();
 			$("#nextButton").text("Return");
@@ -40,6 +46,7 @@ $(document).ready(function(){
 			$("#bottomScript").show();
 			$("#chineseBottom").show();
 			$("#chineseText").show();
+			$("#text2").hide();
 			$("#image").html("");
 			$("#nextButton").text("Next");
 	}
