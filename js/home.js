fa42d7fc56d11fe49cb899414992c914c45d3ccb
Edward Rees <erees.hk@gmail.com>
2017-03-04 20:53:53 +0800

    Many changes
    - Updated Menu, Hidden College Plan
    - Fixed Footer
    - Added ‘button’ features to index page
    - Adjusted font sizes

--- js/home.js
+++ js/home.js
@@ -25,19 +25,23 @@ $(document).ready(function(){
 	
 	function First(){
 			//$("#text2").html("<span>提供舒適優閒及安全的家居生活，是我們服務的首要宗旨 。<br /><br /></span><span id='indent'>我們提供的服務以人為本 。因此，與一般承建商的裝置服務有別，我們提供專業人士作家訪 ，從而了解每使用者的不同個別需要 ，繼而提供各種安全設施，但求讓使用者得到適用的裝置 ，從而在熟悉的環境享受獨立  ，自主  ，及安全無憂的生活。</span><br /><br /><br /><br />");
-			$("#text").html("<span>Your comfort is our passion.  Especially in the home - your home.  <br /><br /></span><span id='indent'>We put people first in our service with an approach differing from normal contracting.  S+ provide professional consultation to understand your special needs, provide a range of safety products accommodating those needs and install them with that customized purpose in your home.   Our aim is to cater for everyone’s differing and personal requirement so they can comfortably enjoy staying in a familiar place - independent and safe.</span><br /><br /><br/><br/> <a href='discussion.html' style='float: right'>Discussion Page</a>");
-			$("#text").css("font-size", "20px");
-			$("#text2").css("font-size", "20px");
-			$("#text2").css("color", "#606060");
-			$("#text2").css("text-align", "left");
-			$("#text2").css("text-indent", "50px");
-			$("#text").css("text-indent", "50px");
-			$("#indent").css("text-indent", "20px");
+			$("#text").html("<br /><span style='font-size: 40px'>Your comfort is our passion.  Especially in the home - <span style='color: #F79646'>your home</span>. </span><br /><br /><span style='font-size: 20px; line-height: 0.5%'>We put people first in our service with an approach differing from normal contracting.  S+ provide professional consultation to understand your special needs, provide a range of safety products accommodating those needs and install them with that customized purpose in your home.   Our aim is to cater for everyone’s differing and personal requirement so they can comfortably enjoy staying in a familiar place - independent and safe.</span><br /><br/><a href='discussion.html' style='float: right; font-size: 25px;'>Discussion Page</a>");
+			//$("#text").css("font-size", "40px");
+			//$("#text2").css("font-size", "20px");
+			//$("#text2").css("color", "#606060");
+			//$("#text2").css("text-align", "left");
+			//$("#text2").css("text-indent", "50px");
+			//$("#text").css("text-indent", "50px");
+			//$("#indent").css("text-indent", "20px");
+			
+			
+		
 			$("#bottomScript").hide();
 			$("#text2").show();
 			$("#chineseBottom").hide();
 			$("#chineseText").hide();
-			$("#nextButton").text("Return");
+			$("#nextButton").text("<< Return");
+			$("#nextButton").css("border-left", "3px");
 			
 	}
 	function Second(){
@@ -49,7 +53,7 @@ $(document).ready(function(){
 			//$("#chineseText").show();
 			$("#text2").hide();
 			$("#image").html("");
-			$("#nextButton").text("Next");
+			$("#nextButton").text("Next >>");
 	}
 })
 
