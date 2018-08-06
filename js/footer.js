5e7d453faec956324c2691d282b7dfcfc97ada2d
Edward Rees <erees.hk@gmail.com>
2018-08-06 11:53:33 +0800

    change of footer

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
 $(document).ready(function(){
-	$("#footer1").html("<nav class='navbar navbar-default navbar-fixed-bottom'><div class='container'><div class='fixed-bottom col-lg-12 text-center'><h1 style='font-size: 16px'><span style='color: #A6A6A6'>S Plus </span><span style='color: #606060'>Group Limited</span></h1><h5 style='font-size: 12px;'><span style='color: #A6A6A6;'>Copyright 2017. All rights reserved</span><span style='color: #F79646'> | </span><span style='color: #A6A6A6;'><a href='#terms' data-toggle='modal' style='color: #a6a6a6';>Terms and Conditions</a></span></h5></div></div></nav>");
+	$("#footer1").html("<nav class='navbar navbar-default navbar-fixed-bottom'><div class='container'><div class='fixed-bottom col-lg-12 text-center'><h1 style='font-size: 16px'><span style='color: #A6A6A6'>S Plus </span><span style='color: #606060'>Group Limited</span></h1><h5 style='font-size: 12px;'><span style='color: #A6A6A6;'>Copyright 2018. All rights reserved</span><span style='color: #F79646'> | </span><span style='color: #A6A6A6;'><a href='#terms' data-toggle='modal' style='color: #a6a6a6';>Terms and Conditions</a></span></h5></div></div></nav>");
 });
 
